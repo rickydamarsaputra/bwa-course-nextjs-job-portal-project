20035767914b930c5b24086d6c11116a3962c67c
rickydamarsaputra <retrocode.rc@gmail.com>
2024-10-07 20:02:35 +0700

    Slicing Clients Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Clients from "@/components/organisms/Clients";
 import Hero from "@/components/organisms/Hero";
 
 export default function Home() {
   return (
     <div className="px-32">
       <Hero/>
+      <Clients/>
     </div>
   );
 }
